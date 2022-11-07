ef98b767cd941d384222a1a771675966b7c81b6e
Dr0Gh0st <bm39502@gmail.com>
2022-11-07 17:36:53 +0000

    Criação do projeto

---
+++ app/src/main/java/com/example/undeadshop/views/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.undeadshop.views;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.undeadshop.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
